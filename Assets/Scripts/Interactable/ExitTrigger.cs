bf8a780497687ede0e719c9b91bd93915999ac05
Logan Olding <logold70@kingshigh.school.nz>
2022-07-28 13:23:06 +1200

    Finished Level 2 Starting on Interactable on button push. Also Added Level swapping via exits.

--- Assets/Scripts/Interactable/ExitTrigger.cs
+++ Assets/Scripts/Interactable/ExitTrigger.cs
@@ -2,16 +2,16 @@ using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
+using UnityEngine.SceneManagement;
 public class ExitTrigger : MonoBehaviour{
+    public string LevelSwap;
 
     private void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             UnityEngine.Debug.Log("EXITLEVEL");
-
+            SceneManager.LoadScene(LevelSwap);
         }
     }
 }
---
+++ Assets/Scripts/InteractionSystem/Interactor.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionPointRadius = 0.5f;
+    [SerializeField] private LayerMask _interactionMask;
+
+    private readonly Collider[] _colliders = new Collider[3];
+    [SerializeField] private int _numFound;
+
+    private void Update() {
+        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactionMask);
+    }
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
+    }
+
+}
