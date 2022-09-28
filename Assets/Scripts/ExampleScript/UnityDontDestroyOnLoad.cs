16d751f1366625798a86cc28e9d0d01b5cefc933
Logan Olding <logold70@kingshigh.school.nz>
2022-09-29 09:53:31 +1300

    Fixed Alive Tree Collision Begun on display item.

---
+++ Assets/Scripts/ExampleScript/UnityDontDestroyOnLoad.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Object.DontDestroyOnLoad example.
+//
+// Two scenes call each other. This happens when OnGUI button is clicked.
+// scene1 will load scene2; scene2 will load scene1. Both scenes have
+// the Menu GameObject with the SceneSwap.cs script attached.
+//
+// AudioSource plays an AudioClip as the game runs. This is on the
+// BackgroundMusic GameObject which has a music tag.  The audio
+// starts in AudioSource.playOnAwake. The DontDestroy.cs script
+// is attached to BackgroundMusic.
+
+public class UnityDontDestroyOnLoad : MonoBehaviour
+{
+    public Texture aTexture;
+
+    private void OnGUI()
+    {
+        int xCenter = (Screen.width / 2);
+        int yCenter = (Screen.height / 2);
+        int width = 200;
+        int height = 200;
+
+        GUIStyle fontSize = new GUIStyle(GUI.skin.GetStyle("button"));
+        fontSize.fontSize = 32;
+        GUI.DrawTexture(new Rect(xCenter - width, yCenter - height, width, height), aTexture, ScaleMode.ScaleToFit, true, 10.0f);
+
+    }
+}
